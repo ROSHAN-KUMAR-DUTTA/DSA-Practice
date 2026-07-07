8100a4b64cda51f8464e15e101c10866b5a92d87
ROSHAN-KUMAR-DUTTA <rk.dutta.28112007@gmail.com>
2026-07-07 13:32:18 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.22%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int reversed=0;
+        while(x!=0){
+            reversed=reversed*10+x%10;
+            x/=10;
+        }
+        if (reversed>INT_MAX || reversed<INT_MIN) return 0;
+        return reversed;
+    }
+};
