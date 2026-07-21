1a52410a86d0654b06d6b5aa168ccc516e5467d4
ROSHAN-KUMAR-DUTTA <rk.dutta.28112007@gmail.com>
2026-07-21 19:57:14 +0530

    Time: 25 ms (29.39%), Space: 17.5 MB (23.42%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
+        if(n<4) return {};
         sort(nums.begin(),nums.end());
     vector<vector<int>> ans;
     for(int i=0;i<n-3;i++){
