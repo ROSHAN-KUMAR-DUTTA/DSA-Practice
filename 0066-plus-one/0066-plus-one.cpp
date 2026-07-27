3d90d61e02e52bbf5e492427cbf7d97c3e7d774b
ROSHAN-KUMAR-DUTTA <rk.dutta.28112007@gmail.com>
2026-07-27 19:54:25 +0530

    Time: 0 ms (100%), Space: 11.7 MB (28.33%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -9,9 +9,6 @@ public:
         }
         digits[i]=0;
     }
-    // digits.push_back(0);
-    // digits[0]=1;
-    //the above two commands can also be written as below
     digits.insert(digits.begin(),1);
     return digits;
     }
