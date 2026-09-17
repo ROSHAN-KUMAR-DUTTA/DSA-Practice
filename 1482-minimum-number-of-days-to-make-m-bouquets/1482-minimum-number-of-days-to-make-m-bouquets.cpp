e3a3e66c53aa3ef7e29e517b0c74e5eda4c302c9
ROSHAN-KUMAR-DUTTA <rk.dutta.28112007@gmail.com>
2026-09-17 22:09:18 +0530

    Time: 15 ms (95.4%), Space: 70.4 MB (80.41%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool possible(vector<int>& arr, long long n, long long days, long long m, long long k) {
-        int count =0;
+    bool possible(vector<int>& arr, int n, int days, int m, int k) {
+        int count = 0;
         int bloomed = 0;
         for (int i = 0; i < n; i++) {
             if (arr[i] <= days) {
@@ -14,13 +14,14 @@ public:
         bloomed += count / k;
         return bloomed >= m;
     }
-    int minDays(vector<int>& arr, long long m, long long k) {
-        long long n=arr.size();
-        if (n < m * k)
+    int minDays(vector<int>& arr, int m, int k) {
+        int n=arr.size();
+        long long check=(long long)m*k;
+        if ((long long)n < check)
             return -1;
-        long long low = *min_element(arr.begin(),arr.end());
-        long long high = *max_element(arr.begin(),arr.end());
-        long long days = 0;
+        int low = *min_element(arr.begin(),arr.end());
+        int high = *max_element(arr.begin(),arr.end());
+        int days = 0;
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (possible(arr, n, mid, m, k)) {
@@ -29,6 +30,6 @@ public:
             } else
                 low = mid + 1;
         }
-        return (int)days;
+        return days;
     }
 };
