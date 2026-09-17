341f55cdffaabbb282a6ed9b7939c7e6de839e0f
ROSHAN-KUMAR-DUTTA <rk.dutta.28112007@gmail.com>
2026-09-17 14:25:01 +0530

    Time: 14 ms (19.7%), Space: 23 MB (24.48%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long findHours(vector<int>& piles, int n, int k) {
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (k + piles[i] - 1) / k;
+        }
+        return ans;
+    }
+
+    long long minEatingSpeed(vector<int>& piles, int h) {
+        long long n = piles.size();
+        long long low = 1;
+        long long high = *max_element(piles.begin(), piles.end());
+        long long ans = LLONG_MAX;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            long long hours = findHours(piles, n, mid);
+            if (hours <= h) {
+                ans = min(ans, mid);
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return ans;
+    }
+};
