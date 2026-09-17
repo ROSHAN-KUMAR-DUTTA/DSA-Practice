b9862cebbcaaafee67702d58e064562acdf5de30
ROSHAN-KUMAR-DUTTA <rk.dutta.28112007@gmail.com>
2026-09-17 10:44:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -10,7 +10,7 @@ public:
             if (mid <= x / mid) {
                 low = mid + 1;
             } else {
-                high = mid - 1;
+                high = mid - 1 ;
             }
         }
         return high;
